9c1717a72368e3f87aac538c2e46455d93c86da1
Norbert Melzer <timmelzer@gmail.com>
2016-03-13 00:36:39 +0100

    Initial commit

---
+++ lib/file_info.ex
@@ -0,0 +1,25 @@
+defmodule FileInfo do
+  @moduledoc """
+  This module is to get some information about files. Currently it just gives
+  you the files MIME-type in a string representation.
+  """
+
+  @doc """
+  Retrieves informations about one or multiple files, using the `file` command
+  line tool which is shipped with most linux distributions.
+  """
+  @spec get_info(Path.t | [Path.t]) :: %{Path.t => String.t}
+  def get_info(names)
+  def get_info(name) when is_binary(name), do: get_info([name])
+  def get_info(names) when is_list(names) do
+    {result, 0} = System.cmd("file", ["--mime-type"|names])
+    result
+    |> String.split("\n")
+    |> Stream.filter(&(&1 !== ""))
+    |> Stream.map(&String.split(&1, ": "))
+    |> Stream.map(&to_tuple/1)
+    |> Enum.into(%{})
+  end
+
+  defp to_tuple([path, mime]), do: {path, String.strip(mime)}
+end
---
+++ test/file_info_test.exs
@@ -0,0 +1,8 @@
+defmodule FileInfoTest do
+  use ExUnit.Case
+  doctest FileInfo
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
