e6ff1a1586534111a026afb3497614f6c3ff78a3
Norbert Melzer <timmelzer@gmail.com>
2019-03-14 08:48:41 +0100

    Add mimetype_parser to application lists
    FIX #6

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule FileInfo.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :mimetype_parser]]
   end
 
   # Dependencies can be Hex packages:
